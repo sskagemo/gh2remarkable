1ecea9406ec73851ae38cd9337499dad44ccd684
Steinar Skagemo <5012227+sskagemo@users.noreply.github.com>
2024-04-08 20:41:45 +0200

    added posting of new issues

--- gh2remarkable/update_issues.py
+++ gh2remarkable/update_issues.py
@@ -1,37 +1,42 @@
-from github import Github
+from github import Github, Issue
+from github import Auth
 
-def create_github_issue(issue_data):
-    # Replace 'YOUR_ACCESS_TOKEN' with your actual GitHub access token
-    access_token = 'YOUR_ACCESS_TOKEN'
-    # Replace 'YOUR_USERNAME' with your actual GitHub username
-    username = 'YOUR_USERNAME'
-    # Replace 'YOUR_REPOSITORY' with your actual GitHub repository name
-    repository = 'YOUR_REPOSITORY'
 
+def create_github_issue(issue_data, user, repo, access_token) -> Issue.Issue:
+    
+    auth = Auth.Token(access_token)
     # Create a PyGithub instance using the access token
-    g = Github(access_token)
+    g = Github(auth=auth)
 
     # Get the repository
-    repo = g.get_user(username).get_repo(repository)
+    repo = g.get_user(user).get_repo(repo)
 
     # Extract the issue data
     title = issue_data['title']
     description = issue_data['description']
     labels = issue_data['labels']
-    assignee = issue_data['assignee']
 
     # Create the issue
-    issue = repo.create_issue(title=title, body=description, labels=labels, assignee=assignee)
+    issue = repo.create_issue(title=title, body=description, labels=labels)
 
-    # Print the URL of the created issue
-    print(f"New issue created: {issue.html_url}")
+    g.close()
+    return issue
 
 # Example usage
-issue_data = {
-    'title': 'Example Issue',
-    'description': 'This is an example issue',
-    'labels': ['bug', 'enhancement'],
-    'assignee': 'octocat'
-}
 
-create_github_issue(issue_data)
+if __name__ == "__main__":
+    import os
+    github_token = os.getenv('GITHUB_TOKEN_GH2REMARKABLE')
+
+
+    issue_data = {
+        'title': 'Add section for new issues in the generated PDF',
+        'description': '''A new section should be added to the generated PDF that indicates that all following text are new issues. 
+        The section should be clearly marked and easy to identify, and also include information about how the issues should be formatted.''',
+        'labels': ['enhancement'],
+    }
+
+    issue = create_github_issue(issue_data, 'sskagemo', 'gh2remarkable', github_token)
+        # Print the URL of the created issue
+
+    print(f"New issue created: {issue.html_url}")
